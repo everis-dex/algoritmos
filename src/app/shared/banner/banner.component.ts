f97c5ff0465335303af13789534490133aef43c4
Dennis Valera García <dennis.valeragarcia@emeal.nttdata.com>
2024-10-21 09:16:31 +0200

    fix: correct some banner issues

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -17,13 +17,6 @@ export class BannerComponent {
   @Output()
   private readonly _changeView = new EventEmitter<void>();
 
-  private _window!: Window;
-
-  public redirectToIniciPage(): void {
-    this._window.location.href =
-      'https://administraciodigital.gencat.cat/ca/inici/';
-  }
-
   public redirectToHomeView(): void {
     this._changeView.emit();
   }
