bd7bd7fcb5b224d4320d0cad79adeed4198c5304
Dennis Valera García <dennis.valeragarcia@emeal.nttdata.com>
2024-10-25 12:20:30 +0200

    feat: start development

--- src/app/pages/systems-searcher/components/search-filters/search-filters.component.ts
+++ src/app/pages/systems-searcher/components/search-filters/search-filters.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-filters',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-filters.component.html',
-  styleUrl: './search-filters.component.scss'
+  styleUrl: './search-filters.component.scss',
 })
 export class SearchFiltersComponent {
+  public toggleStates: Record<string, { display: boolean; rotation: boolean }> =
+    {
+      'category-list': { display: false, rotation: false },
+      'tag-list': { display: false, rotation: false },
+      'state-list': { display: false, rotation: false },
+      'algorithm-type-list': { display: false, rotation: false },
+    };
 
+  public toggle(id: string): void {
+    this.toggleStates[id] = {
+      display: !this.toggleStates[id].display,
+      rotation: !this.toggleStates[id].rotation,
+    };
+  }
 }
--- src/app/pages/systems-searcher/systems-searcher.component.ts
+++ src/app/pages/systems-searcher/systems-searcher.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
+import { SearchFiltersComponent } from './components/search-filters/search-filters.component';
 
 @Component({
   selector: 'app-systems-searcher',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, SearchFiltersComponent],
   templateUrl: './systems-searcher.component.html',
   styleUrl: './systems-searcher.component.scss',
 })
