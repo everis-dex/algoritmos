49d223e4037eb0af2dfd1a872e7fed6afbb745e5
Dennis Valera García <dennis.valeragarcia@emeal.nttdata.com>
2024-11-14 13:20:34 +0100

    feat: set component

--- src/app/pages/system-detail/system-detail.component.ts
+++ src/app/pages/system-detail/system-detail.component.ts
@@ -2,11 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BasicDataComponent } from './components/basic-data/basic-data.component';
 import { AlgorithmicSystemCard } from '../../interfaces/cards';
 import { TabsDataComponent } from './components/tabs-data/tabs-data.component';
+import { MoreInformationCardComponent } from './components/more-information-card/more-information-card.component';
 
 @Component({
   selector: 'app-system-detail',
   standalone: true,
-  imports: [BasicDataComponent, TabsDataComponent],
+  imports: [
+    BasicDataComponent,
+    TabsDataComponent,
+    MoreInformationCardComponent,
+  ],
   templateUrl: './system-detail.component.html',
   styleUrl: './system-detail.component.scss',
 })
