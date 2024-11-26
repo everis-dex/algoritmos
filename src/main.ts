fe4adc6375c5622f32702ce6287c47622d51f7aa
Sara Belen Mozaffary Aliste <smozaffa@emeal.nttdata.com>
2024-11-26 12:08:36 +0100

    fix: url typo and unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from './app/app.component';
 
-function getLang() {
-  const hash = window.location.hash; // Ejemplo: "#googtrans(ca|it)"
-  if (hash.startsWith('#googtrans')) {
-      const languages = hash.match(/\((.*?)\)/)?.[1];
-      if (languages) {
-        const [sourceLang, targetLang] = languages.split('|');
-        return targetLang;
-    } else {
-        return 'ca';
-    }
-  } else {
-      return 'ca';
-  }
-}
-
-
 function bootstrapGecat() {
-  const lang = getLang();
-  console.log(' ~ lang:', lang)
 
-  fetch(`https://administraciodigital.gencat.cat/template?mode=html&code=OAD0001&idioma=ca}&nocache`) // Cambia por tu endpoint real
+  fetch(`https://administraciodigital.gencat.cat/template?mode=html&code=OAD0001&idioma=ca&nocache`)
   .then(response => response.text())
   .then(html => {
     // Sobrescribimos el contenido del documento
