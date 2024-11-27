7ec526a041c947c188ee71d38e91f718b37d30d7
Dennis Valera García <dennis.valeragarcia@emeal.nttdata.com>
2024-11-27 14:07:24 +0100

    fix: remove some unuseful imports and set new switch format in html

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,10 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
