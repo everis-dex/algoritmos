930fb41b8e45a0aadc3dfa64c00c000422d3e8e5
Dennis Valera García <dennis.valeragarcia@emeal.nttdata.com>
2024-11-27 14:12:58 +0100

    fix: format file

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
-import { bootstrapApplication } from "@angular/platform-browser";
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 function bootstrapGencat() {
-
-  fetch(`https://administraciodigital.gencat.cat/template?mode=html&code=OAD0001&idioma=ca&nocache`)
-  .then(response => response.text())
-  .then(html => {
-    // Sobrescribimos el contenido del documento
-    document.open();
-    document.write(html);
-    document.close();
-    setTimeout(() => {
-      const main = document.getElementById('main');
-      if (main) {
-        const appRoot = document.createElement('app-root');
-        main.appendChild(appRoot);
-      }
-      bootstrapApplication(AppComponent).catch(err =>
-        console.error('Error al inicializar Angular:', err)
-      );
-    }, 1000);
-  })
-  .catch(err => {
-    console.error('Error al cargar el index.html dinámico:', err);
-  });
-
+  fetch(
+    `https://administraciodigital.gencat.cat/template?mode=html&code=OAD0001&idioma=ca&nocache`
+  )
+    .then((response) => response.text())
+    .then((html) => {
+      // Sobrescribimos el contenido del documento
+      document.open();
+      document.write(html);
+      document.close();
+      setTimeout(() => {
+        const main = document.getElementById('main');
+        if (main) {
+          const appRoot = document.createElement('app-root');
+          main.appendChild(appRoot);
+        }
+        bootstrapApplication(AppComponent).catch((err) =>
+          console.error('Error al inicializar Angular:', err)
+        );
+      }, 1000);
+    })
+    .catch((err) => {
+      console.error('Error al cargar el index.html dinámico:', err);
+    });
 }
 
 bootstrapGencat();
