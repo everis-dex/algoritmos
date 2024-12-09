a232fc69b25a8432e9ab58de9131ceec5e02d14c
Sara Belen Mozaffary Aliste <smozaffa@emeal.nttdata.com>
2024-12-09 15:35:04 +0100

    fix: improves template fetch, replaces all main content, fix styles

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,27 @@ function bootstrapGencat() {
       document.open();
       document.write(html);
       document.close();
-      setTimeout(() => {
-        const main = document.getElementById('main');
-        if (main) {
-          const appRoot = document.createElement('app-root');
-          main.appendChild(appRoot);
-        }
-        bootstrapApplication(AppComponent, appConfig).catch((err) =>
-          console.error('Error al inicializar Angular:', err)
-        );
-      }, 2000);
+
+      document.addEventListener('DOMContentLoaded', () => {
+        const observer = new MutationObserver(() => {
+          const main = document.getElementById('main');
+          if (main) {
+            observer.disconnect();
+            main.innerHTML = ''; // eliminar línea si el contenido debe añadirse a main en lugar de reemplazar todo el contenido
+            const appRoot = document.createElement('app-root');
+            main.appendChild(appRoot);
+
+            bootstrapApplication(AppComponent, appConfig).catch((err) =>
+              console.error('Error al inicializar Angular:', err)
+            );
+          }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+      });
     })
     .catch((err) => {
-      console.error('Error al cargar el index.html dinámico:', err);
+      console.error('Error al cargar el HTML dinámico:', err);
     });
 }
 
